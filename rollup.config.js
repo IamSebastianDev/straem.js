8902e5b1876ce487afb968b2e7eb65a3f5b664c5
Sebastian <sebastian@iamsebastian.dev>
2022-02-19 20:15:44 +0100

    PROG: Added a IIFE bundle for cdn delivery via unpkg

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,35 @@
 /** @format */
 
-import { terser } from 'rollup-plugin-terser';
+import { terser } from "rollup-plugin-terser";
 
 export default [
-	{
-		input: './src/index.js',
-		output: [
-			{
-				file: './dist/index.esm.js',
-				format: 'esm',
+  {
+    input: "./src/index.js",
+    output: [
+      {
+        file: "./dist/index.esm.js",
+        format: "esm",
 
-				sourcemap: true,
-			},
-			{
-				file: './dist/index.js',
-				format: 'cjs',
-				plugins: [terser({ module: false, toplevel: true })],
-				sourcemap: true,
-			},
-		],
-	},
+        sourcemap: true,
+      },
+      {
+        file: "./dist/index.js",
+        format: "cjs",
+        plugins: [terser({ module: false, toplevel: true })],
+        sourcemap: true,
+      },
+
+      /**
+       * The IIFE bundle is used for delivery via unpgk cdn and as the browser package file. It will work in any
+       * browser, even if it doesn't support ESM Modules, imports or exports.
+       */
+
+      {
+        file: "./dist/index.browser.min.js",
+        format: "iife",
+        plugins: [terser()],
+        sourcemap: true,
+      },
+    ],
+  },
 ];

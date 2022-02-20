6f452ec2c5ab63f26e1ce436d85a5b605a360b10
Sebastian <sebastian@iamsebastian.dev>
2022-02-20 19:35:24 +0100

    PROG: Rebuild package and types

--- dist/types/index.esm.d.ts
+++ dist/types/index.esm.d.ts
@@ -1,25 +1,33 @@
 /** @format */
 /**
+ * The dispatch method is used to create and dispatch a CustomEvent.
  *
- * @param  {...string} eventNames
+ * @param  {...string} eventNames - Any number of strings that describes the names of the
+ * CustomEvents to dispatch.
+ * @returns { { eventNames: string[], eventTarget: EventTarget, with: function, from: function } } an object
+ * containing the eventName/s, eventTarget, the 'from()' method to set the eventTarget and the the 'with()'
+ * method to dispatch the event with a given payload
  */
 export function dispatch(...eventNames: string[]): {
     eventNames: string[];
-    with(payload: any, eventInit?: {}): void;
-    eventTarget: Window & typeof globalThis;
-    from(target: any): {
-        eventTarget: Window & typeof globalThis;
-        from(target: any): any;
-    };
+    eventTarget: EventTarget;
+    with: Function;
+    from: Function;
 };
 /** @format */
-export function receive(...eventNames: any[]): {
-    eventNames: any[];
-    then(callback: any, options: any): () => void;
-    eventTarget: Window & typeof globalThis;
-    from(target: any): {
-        eventTarget: Window & typeof globalThis;
-        from(target: any): any;
-    };
+/**
+ * The dispatch method is used to create a custom eventListener.
+ *
+ * @param  {...string} eventNames - Any number of strings that describes the names of the
+ * CustomEvents/Events to listen to.
+ * @returns { { eventNames: string[], eventTarget: EventTarget, with: function, then: function } } an object
+ * containing the eventName/s, eventTarget, the 'from()' method to set the eventTarget and the the 'then()'
+ * method to execute a given callback when the event is received.
+ */
+export function receive(...eventNames: string[]): {
+    eventNames: string[];
+    eventTarget: EventTarget;
+    with: Function;
+    then: Function;
 };
 //# sourceMappingURL=index.esm.d.ts.map
